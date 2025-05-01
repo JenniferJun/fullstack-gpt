e463869cb11e94a26b61c338bf36f4876d143e17
JenniferJun <jenniferjun.au@gmail.com>
2025-05-01 11:31:20 +0900

    Assignment 06

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🤖",
+)
+
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [O] [DocumentGPT(Assignment)](/DocumentGPT) 
+- [x] [QuizGPT(Assignment)](/QuizGPT)
+- [x] [SiteGPT(Assignment)](/SiteGPT) 
+- [x] [Investor Assistant](/InvestorAssistantGPT)
+- [x] [Research Assistant(Assignment)](/ResearchAssistantGPT) 
+"""
+)
